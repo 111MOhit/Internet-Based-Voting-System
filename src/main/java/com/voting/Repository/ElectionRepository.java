5491ec740bf1d76fd72e3810c41acfa4154c649a
Mohit <mohitpotdar111@gmail.com>
2024-10-10 14:14:20 +0530

    Check Repo

--- src/main/java/com/voting/Repository/ElectionRepository.java
+++ src/main/java/com/voting/Repository/ElectionRepository.java
@@ -1,12 +1,14 @@
 package com.voting.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.voting.Entity.ElectionEntity;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface ElectionRepository extends JpaRepository<ElectionEntity, Long> {
     List<ElectionEntity> findByStartDateAfterAndEndDateBefore(LocalDate startDate, LocalDate endDate); 
     int a= 5;
--- src/main/java/com/voting/Repository/UserRepository.java
+++ src/main/java/com/voting/Repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.voting.Repository;
 
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username); // Find user by username
     Boolean existsByUsername(String username); // Check if username exists
