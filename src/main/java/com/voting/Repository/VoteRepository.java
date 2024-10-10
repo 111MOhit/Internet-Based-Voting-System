7bc7ce74518d92bd1fc2526b28bdaaac37dee7a3
Mohit <mohitpotdar111@gmail.com>
2024-10-10 13:53:53 +0530

    Modified Repo

--- src/main/java/com/voting/Repository/VoteRepository.java
+++ src/main/java/com/voting/Repository/VoteRepository.java
@@ -1,11 +1,14 @@
 package com.voting.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.voting.Entity.VoteEntity;
 
 import java.util.Optional;
 
+
+@Repository
 public interface VoteRepository extends JpaRepository<VoteEntity, Long> {
     Optional<VoteEntity> findByVoterIdAndElectionId(Long voterId, Long electionId); // Check if a user has voted in an election
     Long countByCandidateId(Long candidateId); // Count votes for a candidate
